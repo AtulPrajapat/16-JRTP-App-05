6414bd82d39c763358dc107bacc072e16406e5ac
AtulPrajapat <atulprajapat77@gmail.com>
2021-07-22 21:41:41 +0530

    featurs-301

--- src/main/java/in/atul/GitEx9.java
+++ src/main/java/in/atul/GitEx9.java
@@ -7,6 +7,11 @@ public class GitEx9 {
 		public void main() {
 			System.out.println("I am from main");
 		}
+
+       public void featurs1(){
+		   System.out.println("i am from featurs-301");
+	   }   
+
 		
 	}
 
